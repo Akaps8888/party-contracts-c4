8a409014cfcf53962c4380ddb9ad3ca4aaa96093
jungsooyun <yunj415@gmail.com>
2023-11-22 18:04:08 +0900

    openzeppelin collision fix

--- contracts/vendor/solmate/ERC4907.sol
+++ contracts/vendor/solmate/ERC4907.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin/contracts/utils/Counters.sol";
 import "../../tokens/IERC4907.sol";
 
 contract ERC4907 is ERC721, IERC4907 {
